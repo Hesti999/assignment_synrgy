502909892cb25e5d1315f078c2b93546cef86de4
Hesti999 <hestipratiwi999@gmail.com>
2022-01-18 19:09:45 +0700

    modified assign 2

--- assignment2.js
+++ assignment2.js
@@ -1,21 +1,22 @@
-// input banyak barang yang dibeli
-const jumBar = prompt("Masukan jumlah barang yang dibeli");
+// Input banyak barang yang dibeli
+var jumBar = parseInt(prompt("Masukan jumlah barang yang dibeli"));
+// Inisialisasi variabel hasil & harga
 var hasil = "";
-var totHarga;
-// menentuka decision
-// jumlah barang > 5 melakukan proses penjumlahan total harga
-if (jumBar > 5) {
-  for (let i = 1; i <= jumBar; i++) {
-    let Harg = prompt("Masukan harga tiap barang: ");
-    totHarg += Harg;
-  }
-  hasil = "diskon 10 % dari" + totHarg;
-  //   kurang dari 5, harga normal
-} else {
-  hasil = "harga normal";
-}
-// menentukan decision, apabila total harga lebih dari 100k diskon 10% jika kurang 100k diskon 5%
-hasil = totHarg > 100.0 ? "Diskon 10 %" : "Diskon 5 %";
+var totalHarga = 0;
 
-// menunjukan nilai diskon
-alert(hasil);
+// Menentukan kondisi pada jumlah barang
+// Jika barang > 5 maka dilakukan perulangan total harga
+if (jumBar > 5) {
+  for (var i = 1; i <= jumBar; i++) {
+    var harga = parseInt(prompt("Masukan harga tiap barang: ", "Masukan harga dengan nilai > 10.000"));
+    totalHarga = totalHarga + harga;
+  }
+  // Menentukan kondisi jika harga > 100.000 maka mendapatkan diskon 10% jika tidak maka diskon 5%
+  hasil = totalHarga > 100000 ? "Diskon 10 %" : "Diskon 5 %";
+  // Barang < 5 hasil = Harga normal
+} else {
+  hasil = "Harga normal";
+}
+
+// Output
+alert(`${hasil}`);
